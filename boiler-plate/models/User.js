5d0328a487f5f795e89a171d0737b914861b88d3
SeraWi <wsr2357@naver.com>
2022-04-21 22:48:06 +0900

    boiler-plate : mongo DB , User schema create

--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -9,68 +9,68 @@ const saltRounds = 10; //10자리로 암호 생성
 
 
 const userSchema = mongoose.Schema({
-    name :{
+    name: {
         type: String,
-        maxlength : 50
+        maxlength: 50
     },
-    email :{
-        type:String,
-        trim : true,
-        unique : 1
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
     },
-    password:{
-        type : String,
+    password: {
+        type: String,
         maxlength: 5
     },
-    lastname:{
-        type:String,
-        maxlength : 50
+    lastname: {
+        type: String,
+        maxlength: 50
     },
-    role:{
+    role: {
         type: Number,
-        default : 0
+        default: 0
     },
-    image : String,
-    token :{
-        type:String
+    image: String,
+    token: {
+        type: String
     },
-    tokenExp :{
+    tokenExp: {
         type: Number
     }
 
 })
 
 //save하기 전에 시행하기
-userSchema.pre('save', function(next){
+userSchema.pre('save', function (next) {
     var user = this;
 
     // 비밀번호가 변경될때만 -> 암호화 시키기
-    if(user.isModified('password')){
+    if (user.isModified('password')) {
         //비밀번호 암호화
 
-        
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-                
-            if(err){
+
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+
+            if (err) {
                 return next(err)
             }
 
             //salt 제대로 생성
             //myPlaintextPassword : 순수한 비번(입력한)
-            bcrypt.hash(user.password, salt, function(err, hash) {
+            bcrypt.hash(user.password, salt, function (err, hash) {
                 // Store hash in your password DB.
                 // hash  : 암호화된 비번
 
-                if(err) return next(err)
-                user.password =hash //기존의 password 를 hash 비번으로 바꿔준다.
+                if (err) return next(err)
+                user.password = hash //기존의 password 를 hash 비번으로 바꿔준다.
                 next()
             }) // bcrpt end
         })// genSalt end
 
     } //if end
-    
+
 })
 
 const User = mongoose.model('User', userSchema)
 
-module.exports ={ User }
+module.exports = { User }
