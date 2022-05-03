093b3bf21a27696531bdd8d663b61130f68e437e
SeraWi <wsr2357@naver.com>
2022-05-03 09:39:35 +0900

    file system 사용하기

---
+++ Node.js/Lesson3/fs.js
@@ -0,0 +1,27 @@
+
+//파일시스템
+const fs = require('fs').promises;
+
+fs.readFile('./readme.txt' , (err, data) =>{
+    if(err){
+        throw err;
+    }
+    console.log(data); //2진법, 16진법, 컴퓨터언어
+    console.log(data.toString());
+})
+
+fs.readFile('./readme.txt').then((data)=>{
+    console.log(data);
+    console.log(data.toString());
+})
+.catch((err) =>{
+    throw err;
+})
+
+fs.writeFile('./writeme.txt', '글이 입력됩니다.')
+.then(() =>{
+    return fs.readFile('./writeme.txt');
+})
+.then((data) =>{
+    console.log(data.toString());
+})
